b9dbe8b30521d0909109c6c3ffeae1144770731d
zhibekm <65110060+zhibekm@users.noreply.github.com>
2021-05-07 01:03:04 +0600

    Update MavenWrapperDownloader.java

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-present the original author or authors.
+ * Copyright 2007-present the original author or authors. 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,7 +91,7 @@ public class MavenWrapperDownloader {
             System.out.println("- Error downloading");
             e.printStackTrace();
             System.exit(1);
-        }
+        } 
     }
 
     private static void downloadFileFromURL(String urlString, File destination) throws Exception {
